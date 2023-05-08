72fd98bdab20d90f2b0f8ad5360ee0297c078520
Nay Min Ko <nmk.freelance@aleeas.com>
2023-05-08 20:13:16 +0630

    Add AutoFill Credential Provider target using a plugin  (#1)
    Created a plugin to add an AutoFill Credential Provider target to iOS app.

---
+++ packages/cordova/supplementary source/cordova_plugin/Autofill_target_iOS/add_autofill_target.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const { execSync, execFileSync } = require("child_process");
+
+module.exports = function (context) {
+    let cordovaRoot = context.opts.projectRoot;
+    let projectRoot = path.join(cordovaRoot, "platforms/ios");
+    let pluginRoot = context.opts.plugin.dir;
+    let projectFile = path.join(cordovaRoot, "platforms/ios/Padloc.xcodeproj/project.pbxproj");
+    let patchScript = path.join(cordovaRoot, "supplementary source/cordova_plugin/Autofill_target_iOS/patch.sh");
+    let diffRoot = path.join(cordovaRoot, "supplementary source/cordova_plugin/Autofill_target_iOS/diff");
+    let patchFile = path.join(cordovaRoot, "supplementary source/Patch/Plugins/Fingerprint.swift.patch");
+    let fingerprintFile = path.join(projectRoot, "Padloc/Plugins/cordova-plugin-fingerprint-aio/Fingerprint.swift");
+
+    // check
+    let projectData = fs.readFileSync(projectFile).toString();
+    let targetUuid = "63E4AB572A08DBFD00C29FAA";
+    if (projectData.includes(targetUuid)) {
+        return;
+    }
+
+    // create group folder
+    let groupName = "Autofill";
+    let autofillGroupPath = path.join(projectRoot, groupName);
+    if (!fs.existsSync(autofillGroupPath)) {
+        fs.mkdirSync(path.join(projectRoot, groupName));
+    }
+
+    // copy entitlements files
+    let padlocFolder = path.join(projectRoot, "Padloc");
+    let resRoot = path.join(pluginRoot, "res");
+
+    for (file of ["Entitlements-Debug.plist", "Entitlements-Release.plist"]) {
+        fs.copyFileSync(path.join(resRoot, file), path.join(padlocFolder, file));
+    }
+
+    // patch project
+    let out = execFileSync(patchScript, [projectFile, diffRoot]);
+    fs.writeFileSync(projectFile, out);
+
+    // patch fingerprint
+    execSync(`patch "${fingerprintFile}" "${patchFile}"`);
+};
